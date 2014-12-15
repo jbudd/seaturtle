b6df70106c7f2d71101acd527b39bfcbf7b97612
Johnathan Budd <jbudd01@college.harvard.edu>
2014-12-15 11:20:16 -0500

    added inputs

--- test_motor.py
+++ test_motor.py
@@ -11,18 +11,14 @@ GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
 
-
+speed = raw_input("Please enter a speed:")
 print "Turning motor on"
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 p = GPIO.PWM(Motor1E,500)
+p.start(float(speed))
 
-if not sys.argv[1]:
-	p.start(50)
-else:
-	p.start(float(sys.argv[1]))
-
-raw_input()
+raw_input("Press any key to stop the motor")
 
 print "stopping motor"
 
