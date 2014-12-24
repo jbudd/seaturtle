a40c18a17ac39867d5389674d4c81eeca3ae0bea
Johnathan Budd <jbudd01@college.harvard.edu>
2014-12-23 16:04:11 -0800

    updated camera

--- test_camera.py
+++ test_camera.py
@@ -1,5 +1,17 @@
 import picamera
+import subprocess
 
 camera = picamera.PiCamera()
 
-camera.capture('image.png')
+camera.capture('image2.png')
+
+proc = subprocess.Popen('../build/bin/apriltags_demo' + ' image2.png',
+	shell=True,
+	stdout = subprocess.PIPE,
+	stderr = subprocess.PIPE,
+	stdin=subprocess.PIPE)
+
+so, se = proc.communicate()
+
+print  "this is the return" + so 
+print se
